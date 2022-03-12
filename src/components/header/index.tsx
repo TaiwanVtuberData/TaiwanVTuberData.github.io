3d37912aa10c02228e77cc339aac2a78e97e9e5e
nh60211as <nh60211@gmail.com>
2022-03-12 22:57:45 +0800

    Fix deployed site link to wrong location
    Deployed site (https://nh60211as.github.io/TaiwanVtuberData-Preact/)
    has extra prefix after host name.
    Fix this issue by using different URL prefix when in development.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,6 +3,10 @@ import { Link } from 'preact-router/match';
 import style from './style.css';
 
 const Header: FunctionalComponent = () => {
+  // use default URL prefix (no prefix) if in development
+  const SITE_URL_PREFIX: string =
+    process.env.NODE_ENV === 'production' ? '/TaiwanVtuberData-Preact' : '';
+
   const LinkElement = (text: string, linkTo: string): h.JSX.Element => {
     return (
       <div class={style.gridItem}>
@@ -16,17 +20,26 @@ const Header: FunctionalComponent = () => {
       <h1 class={style.title}>臺灣 VTuber 列表 (目前無內容)</h1>
       <nav class={style.navGrid}>
         {[
-          { text: '首頁', linkTo: '/' },
-          { text: '重大活動月曆', linkTo: '/event-calendar' },
-          { text: '所有 VTuber', linkTo: '/all-vtubers' },
-          { text: '團體列表', linkTo: '/group-calendar' },
-          { text: '熱門 VTuber', linkTo: '/trending-vtubers' },
-          { text: '熱門影片', linkTo: '/trending-videos' },
-          { text: '成長中 VTuber', linkTo: '/growing-vtubers' },
-          { text: '近期出道', linkTo: '/debut-vtubers' },
-          { text: '近期畢業', linkTo: '/graduate-vtubers' },
-          { text: '資料登載/錯誤回報', linkTo: '/report-issue' },
-          { text: '關於', linkTo: '/about' },
+          { text: '首頁', linkTo: `${SITE_URL_PREFIX}/` },
+          { text: '重大活動月曆', linkTo: `${SITE_URL_PREFIX}/event-calendar` },
+          { text: '所有 VTuber', linkTo: `${SITE_URL_PREFIX}/all-vtubers` },
+          { text: '團體列表', linkTo: `${SITE_URL_PREFIX}/group-calendar` },
+          {
+            text: '熱門 VTuber',
+            linkTo: `${SITE_URL_PREFIX}/trending-vtubers`,
+          },
+          { text: '熱門影片', linkTo: `${SITE_URL_PREFIX}/trending-videos` },
+          {
+            text: '成長中 VTuber',
+            linkTo: `${SITE_URL_PREFIX}/growing-vtubers`,
+          },
+          { text: '近期出道', linkTo: `${SITE_URL_PREFIX}/debut-vtubers` },
+          { text: '近期畢業', linkTo: `${SITE_URL_PREFIX}/graduate-vtubers` },
+          {
+            text: '資料登載/錯誤回報',
+            linkTo: `${SITE_URL_PREFIX}/report-issue`,
+          },
+          { text: '關於', linkTo: `${SITE_URL_PREFIX}/about` },
         ].map((e) => LinkElement(e.text, e.linkTo))}
       </nav>
     </header>
