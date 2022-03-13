72339b77c0a6267ced32dce3375f7b6134404a38
nh60211as <nh60211@gmail.com>
2022-03-13 22:12:23 +0800

    Updates

--- route-home.chunk.5f1d9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{FDtd:function(e,n,o){"use strict";o.r(n);var t=o("hosL"),c="home__uRcqa";n.default=function(){return Object(t.h)("div",{class:c},Object(t.h)("h1",null,"Home"),Object(t.h)("p",null,"This is the Home component."))}}}]);
-//# sourceMappingURL=route-home.chunk.5f1d9.js.map
---
+++ route-notfound.chunk.b132a.esm.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3LGf":function(e,t,c){"use strict";c.r(t);var h=c("hosL"),n=c("JBDq"),o="notfound__Ecgkh";t.default=()=>Object(h.h)("div",{class:o},Object(h.h)("h1",null,"Error 404"),Object(h.h)("p",null,"That page doesn't exist."),Object(h.h)(n.a,{href:"/"},Object(h.h)("h4",null,"Back to Home")))}}]);
+//# sourceMappingURL=route-notfound.chunk.b132a.esm.js.map
