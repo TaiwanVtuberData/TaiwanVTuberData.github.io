a008d8784bae9ea829c63fd56946c03d4236e166
nh60211as <nh60211@gmail.com>
2022-03-13 10:50:59 +0800

    Add mock data table of AllVTubers

--- src/routes/AllVTubers/index.tsx
+++ src/routes/AllVTubers/index.tsx
@@ -1,8 +1,66 @@
 import { FunctionalComponent, h } from 'preact';
-import style from './style.css';
+import DataTable from 'react-data-table-component';
+import ProfileElement from '../../components/ProfileElement';
 
 const AllVTubersPage: FunctionalComponent = () => {
-  return <div class={style.profile}>all</div>;
+  const columns = [
+    {
+      name: '名稱',
+      selector: (row: { profile: unknown }): unknown => row.profile,
+    },
+    {
+      name: 'YouTube 訂閱人數',
+      selector: (row: { YouTubeSubscriberCount: number }): number =>
+        row.YouTubeSubscriberCount,
+      sortable: true,
+    },
+    {
+      name: '團體',
+      selector: (row: { group: string }): string => row.group,
+    },
+    {
+      name: '國家',
+      selector: (row: { nationality: string }): string => row.nationality,
+    },
+  ];
+
+  interface VTuberData {
+    id: string;
+    profile: unknown;
+    YouTubeSubscriberCount: number;
+    group: string;
+    nationality: string;
+  }
+
+  const data: Array<VTuberData> = [
+    {
+      id: '1',
+      profile: ProfileElement({
+        name: '杏仁ミル',
+        imgUrl:
+          'https://yt3.ggpht.com/CRhdIqMTAjxrPJJcpKYEY_Yot20ByMcBid-nsSCcl8YFRCi2wzS_oQZ0bgx490bodZPZVqvDOQ=s88-c-k-c0x00ffffff-no-rj',
+        YouTubeUrl: 'https://www.youtube.com/channel/UCFahBR2wixu0xOex84bXFvg',
+        TwitchUrl: 'https://www.twitch.tv/vtuber_amiru',
+      }),
+      YouTubeSubscriberCount: 380000,
+      group: '',
+      nationality: 'TW',
+    },
+    {
+      id: '2',
+      profile: ProfileElement({
+        name: '李李鈴蘭',
+        imgUrl:
+          'https://yt3.ggpht.com/y_VXsJfNp8sTQCB0lFemCleWB-9HpZ9R2pk7wbCOGrvkpFS3Qa1RAXqUVpgb0NNN9_xzWT446A=s88-c-k-c0x00ffffff-no-rj',
+        YouTubeUrl: 'https://www.youtube.com/channel/UCxWAL-c1psONO_DfG-cq2iA',
+      }),
+      YouTubeSubscriberCount: 169000,
+      group: '雲際線工作室',
+      nationality: 'TW',
+    },
+  ];
+
+  return <DataTable columns={columns} data={data} />;
 };
 
 export default AllVTubersPage;
