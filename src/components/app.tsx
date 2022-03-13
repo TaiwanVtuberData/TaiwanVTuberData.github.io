fef1c8999f05830fe117f1faffcd1c142b437726
nh60211as <nh60211@gmail.com>
2022-03-14 07:19:45 +0800

    Wrap Router in when compiling

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -7,14 +7,22 @@ import NotFoundPage from '../routes/notfound';
 import Header from './header';
 
 const App: FunctionalComponent = () => {
+  const RouterElement = (): h.JSX.Element | undefined => {
+    if (typeof window !== 'undefined') {
+      return (
+        <Router history={createHashHistory()}>
+          <Home path="/" />
+          <AllVTubersPage path="/all-vtubers" />
+          <NotFoundPage default />
+        </Router>
+      );
+    }
+  };
+
   return (
     <div id="preact_root">
       <Header />
-      <Router history={createHashHistory()}>
-        <Home path="/" />
-        <AllVTubersPage path="/all-vtubers" />
-        <NotFoundPage default />
-      </Router>
+      {RouterElement()}
     </div>
   );
 };
