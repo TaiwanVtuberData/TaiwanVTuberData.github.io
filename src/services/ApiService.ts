1ad828051889a80addbd8d2f937bfca590da8fa7
nh60211as <nh60211@gmail.com>
2022-03-15 19:00:05 +0800

    Get latest hash before fetching data from CDN

--- src/services/ApiService.ts
+++ src/services/ApiService.ts
@@ -1,10 +1,41 @@
 import axios, { AxiosResponse } from 'axios';
 import { VTuberDataResponse } from '../types/VTuberData';
 
-const DEPLOY_BASE_URL =
-  'https://cdn.statically.io/gh/nh60211as/TaiwanVtuberTrackingDataJson/5976dd9e256296a3931f2dfddb45eeac11fb62dd' as const;
-axios.defaults.baseURL = `${DEPLOY_BASE_URL}/api/v0`;
+interface CommitDetail {
+  sha: string;
+  date?: string;
+}
+
+let commitDetail: CommitDetail;
+
+const setCommitDetail = async (): Promise<void> => {
+  await axios
+    .get(
+      'https://api.github.com/repos/nh60211as/TaiwanVtuberTrackingDataJson/commits/master'
+    )
+    .then((res) => {
+      commitDetail = {
+        sha: res.data.sha,
+        date: res.data.commit.author.date,
+      };
+    })
+    .catch(() => {
+      commitDetail = {
+        sha: 'master',
+      };
+    });
+
+  axios.defaults.baseURL = `https://cdn.statically.io/gh/nh60211as/TaiwanVtuberTrackingDataJson/${commitDetail.sha}/api/v0`;
+};
+
+const AxiosGetWrapper = async <DataType>(
+  url: string
+): Promise<AxiosResponse<DataType>> => {
+  if (commitDetail === undefined) await setCommitDetail();
+
+  return axios.get<DataType>(url);
+};
 
 export const getVTubers = (): Promise<AxiosResponse<VTuberDataResponse>> => {
-  return axios.get<VTuberDataResponse>('/vtubers.json');
+  return AxiosGetWrapper<VTuberDataResponse>('/vtubers.json');
 };
