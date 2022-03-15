2f50f707fbfaa79ea6b33eca05f151bb75e430ce
nh60211as <nh60211@gmail.com>
2022-03-15 21:23:00 +0800

    Add LanguageOptions test

---
+++ tests/LanguageOptions.test.ts
@@ -0,0 +1,7 @@
+import { LanguageOptions } from '../src/types/LanguageOptions';
+
+describe('LanguageOptions Test', () => {
+    test('Expected language options to be 2', () => {
+        expect(LanguageOptions.length).toBe(2);
+    });
+});
