f6ff708b4893f04e5fcbe283254c4925b0490410
nh60211as <45863601+nh60211as@users.noreply.github.com>
2022-04-19 17:34:33 +0800

    #32 Indicate hidden YouTube subscriber count (#34)
    * #32 Indicate YouTube subscribers is hidden
    
    * #32 Remove unused import

--- src/routes/VTuber/index.tsx
+++ src/routes/VTuber/index.tsx
@@ -1,27 +1,9 @@
 import { Fragment, FunctionalComponent, h } from 'preact';
 import { useEffect, useState } from 'preact/hooks';
-import { Text } from 'preact-i18n';
-import DataTable, { TableColumn } from 'react-data-table-component';
-import baseroute from '../../baseroute';
-import SearchBar from '../../components/SearchBar';
 import { Dictionary } from '../../i18n/Dictionary';
 import * as Api from '../../services/ApiService';
-import DefaultDataTableProps from '../../utils/DefaultDataTableProps';
 import '../../style/index.css';
-import tableStyle from '../../style/DataTableStyle.module.css';
-import ActivityRowStyles from '../../style/ActivityRowStyles';
-import { VTuberDebutDisplayData } from '../../types/TableDisplayData/VTuberDebutDisplayData';
-import { getISODateString } from '../../utils/DateTimeUtils';
-import IsTodayRowStyle from '../../style/IsTodayRowStyles';
-import { VTuberDebutToDisplay } from '../../types/ApiToDisplayData/DebutTransform';
-import QuestionMarkToolTip from '../../components/QuestionMarkToolTip';
-import { openModal } from '../../global/modalState';
-import { VideoInfo } from '../../types/Common/VideoInfo';
-import YouTubeTwitchCount from '../../components/YouTubeTwitchCount';
-import ProfileImageLink from '../../components/ProfileImageLink';
-import { VTuberFullData } from '../../types/ApiData/VTuberFullData';
 import { VTuberFullToDisplay } from '../../types/ApiToDisplayData/FullTransform';
-import Profile from '../../components/Profile';
 import VTuberInformation from '../../components/VTuberInformation';
 import { VTuberDisplayFullData } from '../../types/TableDisplayData/VTuberDisplayFullData';
 
