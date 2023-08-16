22474e7ebe8f5d6c2e69d9f8e23e08a08c65dddb
nh60211as <nh60211@gmail.com>
2023-08-16 21:26:35 +0800

    Remove fields that are in violation of YouTube API policy

--- src/Config.ts
+++ src/Config.ts
@@ -7,8 +7,17 @@ const getEnvOrEmpty = (env: string | undefined): string => {
   return defaultValue;
 };
 
+const getEnvAsBooleanOrFalse = (env: string | undefined): boolean => {
+  const envValue = getEnvOrEmpty(env);
+
+  return envValue === 'true';
+};
+
 export const APP_VERSION = getEnvOrEmpty(process.env.APP_VERSION);
 export const ROUTE_PREFIX = getEnvOrEmpty(process.env.GITHUB_PAGES);
 export const GOOGLE_FORM_URL = getEnvOrEmpty(process.env.GOOGLE_FORM_URL);
 export const GITHUB_ISSUE_URL = getEnvOrEmpty(process.env.GITHUB_ISSUE_URL);
 export const CONTACT_EMAIL = getEnvOrEmpty(process.env.CONTACT_EMAIL);
+export const ENFORCE_YOUTUBE_COMPLIANCE = getEnvAsBooleanOrFalse(
+  process.env.ENFORCE_YOUTUBE_COMPLIANCE
+);
