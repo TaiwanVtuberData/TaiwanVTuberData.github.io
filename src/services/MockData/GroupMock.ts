1372c55f9081883509de8aacec8f18c90d267fcb
nh60211as <nh60211@gmail.com>
2023-08-25 22:28:01 +0800

    Add missing mock data fields

--- src/services/MockData/GroupMock.ts
+++ src/services/MockData/GroupMock.ts
@@ -6,12 +6,16 @@ export const GroupMock: ReadonlyArray<GroupData> = [
     id: '1',
     name: '雲際線工作室',
     popularity: 28416,
+    livestreamPopularity: 10000,
+    videoPopularity: 10000,
     members: CloudHorizonMock,
   },
   {
     id: '2',
     name: '子午計劃',
     popularity: 9832,
+    livestreamPopularity: 5000,
+    videoPopularity: 12000,
     members: MeridianProjectMock,
   },
 ];
