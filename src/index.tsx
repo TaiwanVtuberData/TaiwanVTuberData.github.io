70ab91a30c3681fca39b81d3a85244865af6db0a
nh60211as <nh60211@gmail.com>
2024-01-20 18:01:31 +0800

    Remove miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,5 @@
 import "./style/index.css";
 import { render } from "preact";
 import { App } from "./components/app";
-import { MockService } from "./services/MockService";
-
-if (process.env.NODE_ENV !== "production") {
-  MockService();
-}
 
 render(<App />, document.getElementById("app"));
